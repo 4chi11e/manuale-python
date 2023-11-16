525d53c2dab52a15d86e41df39e246e59bb00ecf
FTormene <francesco.tormene@liceobanfi.eu>
2023-11-16 15:51:39 +0100

    riordino numeraz es, folders, files, elimino roba

--- Soluzioni/3-02-12.py
+++
@@ -1,6 +0,0 @@
-# 3.2.12	Data una lista di stringhe, crea e poi stampa un’unica stringa contenente tutte le stringhe della lista separate da “; “ (puntoevirgola e spazio)
-
-lista = ["casa", "sedia", "cavallo", "andare in bici", "uscire di casa", "che bello!"]
-stringa = "; ".join(lista)
-print(stringa)
-
--- Soluzioni/old/eserciziario-scuola/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-
-    print(f"{numeri} è palindromo")
